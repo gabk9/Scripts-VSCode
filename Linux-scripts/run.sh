41c89971e709dd10506ad2300d2d1d7d34024ae9
Gabriel Oliveira <giane.ga2010@gmail.com>
2026-01-15 00:28:18 -0300

    Update run.sh

--- Linux-scripts/run.sh
+++ Linux-scripts/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-clear
 
 file="$1"
 
@@ -29,49 +28,63 @@ fi
 # Compilation/execution per language
 case "$extension" in
     c)
+	    clear
         echo "Compiling and running C..."
-        gcc "$file" -o "$dirName/$baseName" -lm && "$dirName/$baseName"
+        if [[ -f "$dirName/main.c" ]]; then
+            gcc "$dirName/main.c" "$dirName/libs/terminal.c" "$dirName/libs/CheckCmd.c" "$dirName/libs/utils.c" "$dirName/libs/s_math.c" -o "$dirName/main" -lm && "$dirName/main"
+        else
+            gcc "$file" -o "$dirName/$baseName" -lm && "$dirName/$baseName"
         fi
         ;;
     cpp)
+	    clear
         echo "Compiling and running C++..."
         g++ "$file" -o "$dirName/$baseName" -lm && "$dirName/$baseName"
         ;;
     py)
+	    clear
         echo "Running Python..."
         python3 "$file"
         ;;
     php)
+	    clear
         echo "Running PHP..."
         php "$file"
         ;;
     js)
+	    clear
         echo "Running JavaScript..."
         node "$file"
         ;;
     asm|s)
+	clear
         echo "Assembling and running ASM..."
         nasm -f elf64 "$file" -o "$dirName/$baseName.o" || exit 1
         ld "$dirName/$baseName.o" -o "$dirName/$baseName" || exit 1
         "$dirName/$baseName"
         ;;
     java)
+	    clear
         echo "Compiling and running Java..."
         javac "$file" -d "$dirName" && java -cp "$dirName" "$baseName"
         ;;
     go)
+	    clear
         echo "Compiling and running Go..."
         go build -o "$dirName/$baseName" "$file" && "$dirName/$baseName"
         ;;
     cob|cbl)
+	    clear
         echo "Compiling and running COBOL..."
         cobc -x -o "$dirName/$baseName" "$file" && "$dirName/$baseName"
         ;;
     f90)
+	    clear
         echo "Compiling and running Fortran..."
         gfortran "$file" -o "$dirName/$baseName" && "$dirName/$baseName"
         ;;
     rs)
+	    clear
         echo "Compiling and running Rust..."
         rustc "$file" -o "$dirName/$baseName" && "$dirName/$baseName"
         ;;
